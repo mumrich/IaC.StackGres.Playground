cdb21e197946790e5e9e1838ec7e60ef0ef89ad9
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-07-07 17:50:10 +0200

    define initial stack.

--- cdktf/main.ts
+++ cdktf/main.ts
@@ -1,14 +1,35 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { provider, release } from "@cdktf/provider-helm";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    // define resources here
+    new provider.HelmProvider(scope, id, {
+      kubernetes: {
+        configPath: "~/.kube/config",
+      },
+    });
+
+    new release.Release(scope, id, {
+      name: "stackgres-operator",
+      namespace: "stackgres",
+      chart:
+        "https://stackgres.io/downloads/stackgres-k8s/stackgres/latest/helm/stackgres-operator.tgz",
+      createNamespace: true,
+      set: [
+        {
+          name: "adminui.service.type",
+          value: "ClusterIP",
+          type: "string",
+        },
+      ],
+    });
   }
 }
 
 const app = new App();
 new MyStack(app, "cdktf");
+
 app.synth();
