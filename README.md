e1420d5927a823d21f7afbb5f0941d406fc66d30
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-07-08 02:49:27 +0200

    fully working

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # IaC.StackGres.Playground
- 
+
+```bash
+cd cdktf
+
+# load modules defined in cdktf.json and generate types
+cdktf get
+
+# deploy
+cdktf deploy --auto-approve
+
+# destroy
+cdktf destroy --auto-approve
+```
--- cdktf/main.ts
+++ cdktf/main.ts
@@ -1,22 +1,36 @@
 import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
-import { provider, release } from "@cdktf/provider-helm";
+import { App, TerraformOutput, TerraformStack, TerraformVariable } from "cdktf";
+import { HelmProvider } from "@cdktf/provider-helm/lib/provider";
+import { Release as HelmRelease } from "@cdktf/provider-helm/lib/release";
+import { Shell } from "./.gen/modules/shell";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    new provider.HelmProvider(this, `${id}-helm-provider`, {
+    function idPrefixer(postfix: string): string {
+      return `${id}-${postfix}`;
+    }
+
+    const k8sNamespace = "stackgres";
+    const chartName = "stackgres-operator";
+
+    const chartVersion = new TerraformVariable(this, "chartVersion", {
+      type: "string",
+      default: "1.5.0",
+    });
+
+    new HelmProvider(this, idPrefixer("helm-provider"), {
       kubernetes: {
         configPath: "~/.kube/config",
       },
     });
 
-    new release.Release(this, `${id}-helm-release`, {
-      name: "stackgres-operator",
-      namespace: "stackgres",
-      chart: "stackgres-operator",
-      version: "1.5.0",
+    const helmRelase = new HelmRelease(this, idPrefixer("helm-release"), {
+      name: chartName,
+      namespace: k8sNamespace,
+      chart: chartName,
+      version: chartVersion.value,
       repository:
         "https://stackgres.io/downloads/stackgres-k8s/stackgres/helm/",
       createNamespace: true,
@@ -35,10 +49,58 @@ class MyStack extends TerraformStack {
       wait: true,
       waitForJobs: true,
     });
+
+    new Shell(this, idPrefixer("operator-check"), {
+      dependsOn: [helmRelase],
+      commandUnix: `kubectl wait -n ${k8sNamespace} deployment/stackgres-operator --for condition=Available`,
+      failOnNonzeroExitCode: true,
+    });
+
+    new Shell(this, idPrefixer("restapi-check"), {
+      dependsOn: [helmRelase],
+      commandUnix: `kubectl wait -n ${k8sNamespace} deployment/stackgres-restapi --for condition=Available`,
+      failOnNonzeroExitCode: true,
+    });
+
+    const credentialsUsername = new Shell(
+      this,
+      idPrefixer("credentials-username"),
+      {
+        dependsOn: [helmRelase],
+        commandUnix: `kubectl get secret -n ${k8sNamespace} stackgres-restapi --template '{{ .data.k8sUsername | base64decode }}'`,
+      }
+    );
+
+    const credentialsPassword = new Shell(
+      this,
+      idPrefixer("credentials-password"),
+      {
+        dependsOn: [helmRelase],
+        commandUnix: `kubectl get secret -n ${k8sNamespace} stackgres-restapi --template '{{ .data.clearPassword | base64decode }}'`,
+      }
+    );
+
+    const notes = new Shell(this, idPrefixer("notes"), {
+      dependsOn: [helmRelase],
+      commandUnix: `helm get notes -n ${k8sNamespace} stackgres-operator`,
+    });
+
+    new TerraformOutput(this, "credentials-username", {
+      value: credentialsUsername.stdoutOutput,
+    });
+
+    new TerraformOutput(this, "credentials-password", {
+      value: credentialsPassword.stdoutOutput,
+    });
+
+    new TerraformOutput(this, "notes", {
+      value: notes.stdoutOutput,
+    });
   }
 }
 
 const app = new App();
+
 new MyStack(app, "cdktf");
 
 app.synth();
