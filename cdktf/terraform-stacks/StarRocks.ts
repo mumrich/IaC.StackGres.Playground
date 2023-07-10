fdb12ed9176271a04c6b7552bf9345420a43c6a7
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-07-10 22:31:31 +0200

    use kubectl provider.

--- cdktf/terraform-stacks/StarRocks.ts
+++ cdktf/terraform-stacks/StarRocks.ts
@@ -1,16 +1,7 @@
-import ClusterRoleConfig from "./assets/starrocks-cr";
-import ManifestConfig from "./assets/starrocks-crd";
-import { ClusterRole } from "@cdktf/provider-kubernetes/lib/cluster-role";
-import { ClusterRoleBinding } from "@cdktf/provider-kubernetes/lib/cluster-role-binding";
 import { Construct } from "constructs";
-import { DataKubernetesNamespace } from "@cdktf/provider-kubernetes/lib/data-kubernetes-namespace";
-import { DataKubernetesServiceAccount } from "@cdktf/provider-kubernetes/lib/data-kubernetes-service-account";
-import { Deployment } from "@cdktf/provider-kubernetes/lib/deployment";
-import { KubernetesProvider } from "@cdktf/provider-kubernetes/lib/provider";
-import { Manifest } from "@cdktf/provider-kubernetes/lib/manifest";
-import { Role } from "@cdktf/provider-kubernetes/lib/role";
-import { RoleBinding } from "@cdktf/provider-kubernetes/lib/role-binding";
-import { TerraformStack } from "cdktf";
+import { TerraformIterator, Fn, TerraformStack } from "cdktf";
+import { KubectlProvider } from "../.gen/providers/kubectl/provider";
+import * as kubectl from "../.gen/providers/kubectl";
 
 export default class StarRocks extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -20,179 +11,24 @@ export default class StarRocks extends TerraformStack {
       return `${id}-${postfix}`;
     }
 
-    const k8sNamespace = "starrocks";
-
-    new KubernetesProvider(this, idPrefixer("k8s-provider"), {
+    new KubectlProvider(this, idPrefixer("kubectl-provider"), {
       configPath: "~/.kube/config",
     });
 
-    new Manifest(this, idPrefixer("k8s-crds"), ManifestConfig);
-
-    const ns = new DataKubernetesNamespace(this, idPrefixer("k8s-namespace"), {
-      metadata: {
-        name: k8sNamespace,
-        labels: {
-          "control-plane": "cn-controller-manager",
-        },
-      },
-    });
-
-    new DataKubernetesServiceAccount(this, idPrefixer("k8s-service-account"), {
-      metadata: {
-        name: "starrocks-controller",
-        namespace: ns.metadata.name,
-      },
-    });
-
-    new ClusterRole(this, idPrefixer("k8s-cluster-role"), ClusterRoleConfig);
-
-    new ClusterRoleBinding(this, idPrefixer("k8s-cluster-role-binding"), {
-      metadata: {
-        name: "starrocks-manager-rolebinding",
-      },
-      roleRef: {
-        apiGroup: "rbac.authorization.k8s.io",
-        kind: "ClusterRole",
-        name: "starrocks-manager",
-      },
-      subject: [
+    const yamlfiles =
+      new kubectl.dataKubectlFilenameList.DataKubectlFilenameList(
+        this,
+        idPrefixer("yaml-files"),
         {
-          kind: "ServiceAccount",
-          name: "starrocks-controller",
-          namespace: ns.metadata.name,
-        },
-      ],
-    });
+          pattern: "./assets/*.yaml",
+        }
+      );
 
-    new Role(this, idPrefixer("k8s-role"), {
-      metadata: {
-        name: "starrocks-leader-election-role",
-        namespace: "starrocks",
-      },
-      rule: [
-        {
-          apiGroups: [""],
-          resources: ["configmaps"],
-          verbs: [
-            "get",
-            "list",
-            "watch",
-            "create",
-            "update",
-            "patch",
-            "delete",
-          ],
-        },
-        {
-          apiGroups: ["coordination.k8s.io"],
-          resources: ["leases"],
-          verbs: [
-            "get",
-            "list",
-            "watch",
-            "create",
-            "update",
-            "patch",
-            "delete",
-          ],
-        },
-        {
-          apiGroups: [""],
-          resources: ["events"],
-          verbs: ["create", "patch"],
-        },
-      ],
-    });
+    const fileListIterator = TerraformIterator.fromList(yamlfiles.matches);
 
-    new RoleBinding(this, idPrefixer("k8s-role-binding"), {
-      metadata: {
-        name: "starrocks-leader-election-rolebinding",
-        namespace: ns.metadata.name,
-      },
-      roleRef: {
-        apiGroup: "rbac.authorization.k8s.io",
-        kind: "Role",
-        name: "starrocks-leader-election-role",
-      },
-      subject: [
-        {
-          kind: "ServiceAccount",
-          name: "starrocks-controller",
-          namespace: ns.metadata.name,
-        },
-      ],
-    });
-
-    new Deployment(this, idPrefixer("k8s-deployment"), {
-      metadata: {
-        name: "starrocks-controller",
-        namespace: "starrocks",
-        labels: {
-          "control-plane": "starrocks-controller",
-        },
-      },
-      spec: {
-        selector: {
-          matchLabels: {
-            "control-plane": "starrocks-controller",
-          },
-        },
-        replicas: "1",
-        template: {
-          metadata: {
-            annotations: {
-              "kubectl.kubernetes.io/default-container": "manager",
-            },
-            labels: {
-              "control-plane": "starrocks-controller",
-            },
-          },
-          spec: {
-            securityContext: {
-              runAsNonRoot: false,
-            },
-            container: [
-              {
-                command: ["/sroperator"],
-                args: ["--leader-elect"],
-                image: "starrocks/operator:latest",
-                name: "sroperator",
-                securityContext: {
-                  allowPrivilegeEscalation: false,
-                },
-                livenessProbe: {
-                  httpGet: {
-                    path: "/healthz",
-                    port: "8081",
-                  },
-                  initialDelaySeconds: 15,
-                  periodSeconds: 20,
-                },
-                readinessProbe: {
-                  httpGet: {
-                    path: "/readyz",
-                    port: "8081",
-                  },
-                  initialDelaySeconds: 5,
-                  periodSeconds: 10,
-                },
-                resources: {
-                  limits: {
-                    cpu: "500m",
-                    memory: "128Mi",
-                  },
-                  requests: {
-                    cpu: "10m",
-                    memory: "64Mi",
-                  },
-                },
-              },
-            ],
-            serviceAccountName: "starrocks-controller",
-            terminationGracePeriodSeconds: 10,
-          },
-        },
-      },
+    new kubectl.manifest.Manifest(this, idPrefixer("operator-manifest"), {
+      forEach: fileListIterator,
+      yamlBody: Fn.file(fileListIterator.value),
     });
   }
 }
---
+++ cdktf/terraform-stacks/assets/starrocks/update.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://raw.githubusercontent.com/StarRocks/starrocks-kubernetes-operator/main/deploy/operator.yaml
+wget https://raw.githubusercontent.com/StarRocks/starrocks-kubernetes-operator/main/deploy/starrocks.com_starrocksclusters.yaml
