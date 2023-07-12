fc06b2c65297afd400937330ea2209f761d7497f
Hannes Morgenthaler <hanneszbinden@outlook.com>
2023-07-12 02:13:09 +0200

    add cleanup mechanism

---
+++ full-cleanup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "*** Delecte cluster..."
+k3d cluster delete MyCluster
+
+echo "*** Remove all tfstate-files..."
+rm -f cdktf/*.tfstate cdktf/*.tfstate.backup
+
+echo "*** Remove cdktf.out folder..."
+rm -rf cdktf/cdktf.out
